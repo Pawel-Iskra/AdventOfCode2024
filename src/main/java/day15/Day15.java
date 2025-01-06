0dfbcdc4389d38720033a5250c0e0ffa65dd7b48
Pawel <paweliszcz@wp.pl>
2025-01-06 11:22:14 +0100

    [AoC_2024] day15

---
+++ src/main/java/day15/Day15.java
@@ -0,0 +1,34 @@
+package day15;
+
+import utils.MyUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Day15 {
+
+
+    public static void partOne() {
+
+    }
+
+
+    public static void partTwo() {
+
+    }
+
+
+    private static void prepareData(final List<String> inputLines) {
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String pathToInputFile = "src/main/resources/2024.day15/input.txt";
+        List<String> inputLines = MyUtils.getInputLines(pathToInputFile);
+
+        prepareData(inputLines);
+        partOne();
+        partTwo();
+    }
+}
